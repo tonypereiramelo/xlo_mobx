11f041ffde7c5602d953a505eb18351179cb1d32
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-08-03 13:24:33 -0300

    mudando de paginas do drawer

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
 import 'package:xlo_mobx/screens/home/home_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   BaseScreen({Key? key}) : super(key: key);
 
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
+
+  final PageStore pageStore = GetIt.I<PageStore>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    reaction(
+        (_) => pageStore.page, (int page) => pageController.jumpToPage(page));
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/stores/page_store.dart
@@ -0,0 +1,11 @@
+import 'package:mobx/mobx.dart';
+part 'page_store.g.dart';
+
+class PageStore = _PageStoreBase with _$PageStore;
+
+abstract class _PageStoreBase with Store {
+  @observable
+  int page = 0;
+  @action
+  void setPage(int value) => page = value;
+}
