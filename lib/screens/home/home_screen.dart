b5eb50b700af863bdefc3586b07565d1abacc91d
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-08-03 11:53:01 -0300

    custom_drawer

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:xlo_mobx/components/custom_drawer/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      drawer: CustomDrawer(),
       appBar: AppBar(),
     );
   }
