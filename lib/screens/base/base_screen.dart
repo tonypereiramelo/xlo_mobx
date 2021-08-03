9080eb03ad5d40aae1d6955db3c435a80ba28ef5
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-08-03 11:45:09 -0300

    drawer

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:xlo_mobx/screens/home/home_screen.dart';
 
 class BaseScreen extends StatelessWidget {
-  const BaseScreen({Key? key}) : super(key: key);
+  BaseScreen({Key? key}) : super(key: key);
+
+  final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
         children: <Widget>[
-          Container(
-            color: Colors.green,
-          ),
+          HomeScreen(),
           Container(
             color: Colors.yellow,
           ),
