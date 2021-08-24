746e918912a89a545d6ce56752acc562181ff5d5
tonypereiramelo <69940926+tonypereiramelo@users.noreply.github.com>
2021-08-24 12:25:14 -0300

    test

---
+++ lib/screens/create/create_screen.dart
@@ -0,0 +1,70 @@
+import 'package:brasil_fields/brasil_fields.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:xlo_mobx/components/custom_drawer/custom_drawer.dart';
+
+class CreateScreen extends StatelessWidget {
+  const CreateScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final labelStyle = TextStyle(
+      fontWeight: FontWeight.w800,
+      color: Colors.grey,
+      fontSize: 18,
+    );
+    final contentPadding = const EdgeInsets.fromLTRB(
+      16,
+      10,
+      12,
+      10,
+    );
+    return Scaffold(
+      drawer: CustomDrawer(),
+      appBar: AppBar(
+        title: Text('Criar Anúncio'),
+        centerTitle: true,
+      ),
+      body: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        elevation: 8,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Título *',
+                labelStyle: labelStyle,
+                contentPadding: contentPadding,
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Descrição *',
+                labelStyle: labelStyle,
+                contentPadding: contentPadding,
+              ),
+              maxLines: null,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Preço *',
+                labelStyle: labelStyle,
+                contentPadding: contentPadding,
+                prefixText: 'R\$',
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                RealInputFormatter(centavos: true),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
